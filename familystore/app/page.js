fc32d43a021c4807f0f3769548695f9e07dc337a
Thabang Rammitlwa <thabang451@gmail.com>
2024-10-01 13:33:25 +0200

    Start-up and installing packages

---
+++ familystore/app/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import Head from "next/head";
+
+
+
+export default function Home() {
+  return (
+    
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Head>
+        <link rel="icon"  href="app/assets/favicon/ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+        </Head>
+        <h1 className="text-4xl font-bold text-center sm:text-5xl">hello</h1>
+        </main>
+    </div>
+  );
+}
